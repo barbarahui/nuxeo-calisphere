7d5a023ae6e2548ca734e9228d7b45eaa25b9189
Barbara Hui <barbara.hui@ucop.edu>
2016-01-11 14:17:51 -0800

    Change version in setup.py to 0.0.2

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name = "UCLDC Deep Harvester",
-    version = "0.0.1",
+    version = "0.0.2",
     description = ("deep harvester code for the UCLDC project"),
     long_description=read('README.md'),
     author='Barbara Hui',
