3d7b391d4d3bae744df494c5819ee39506935074
Barbara Hui <barbara.hui@ucop.edu>
2016-01-11 14:00:31 -0800

    Get rid of bad tests.

--- tests/test_deepharvest_nuxeo.py
+++ tests/test_deepharvest_nuxeo.py
@@ -41,9 +41,9 @@ class DeepHarvestNuxeoSimpleTestCase(unittest.TestCase):
    
     def test_fetch_objects_nested(self):
         ''' test fetching of Nuxeo objects for a collection that has nested folders '''
-        dh = deepharvest_nuxeo.DeepHarvestNuxeo('/asset-library/UCR/SabinoOsuna', self.s3_mediajson, pynuxrc=self.pynuxrc)
-        objects = dh.fetch_objects()
-        self.assertEqual(sum(1 for ob in objects), 427) # FIXME bad test! # of objects might change...
+        #dh = deepharvest_nuxeo.DeepHarvestNuxeo('/asset-library/UCR/SabinoOsuna', self.s3_mediajson, pynuxrc=self.pynuxrc)
+        #objects = dh.fetch_objects()
+        #self.assertEqual(sum(1 for ob in objects), 427) # FIXME bad test! # of objects might change...
 
     def test_fetch_components_none(self):
         ''' test getting components for simple object. should be none. '''
@@ -100,9 +100,9 @@ class DeepHarvestNuxeoSimpleTestCase(unittest.TestCase):
 
     def test_get_component_metadata_with_transcript(self):
         ''' test getting metadata for a component that has a transcription '''
-        obj = json.load(open(self.test_component_with_transcript))
-        metadata = self.dh.get_component_metadata(obj)
-        self.assertEqual(metadata['transcription'], u'[written above date]:Stanton Hospital, Washington. City D.C.\n\nThe old year has gone, gone forever with all its joys, sorrows, and cares, a New Year is just began, our hearts beat high with thankfulness and of hope for the future, and we begin the New Year with a merry greeting to all, especially to our absent comrads in the field, may the God of Battles protect all of them and ere another New Years day resore them again to our waiting anxious friends. - a greeting to our bereaved ones with tender sympathy. Those who mourn the loss of these gone forth-to-battle never to return, - to our country in I all levels of Freedom a greeting  with the wish that soon the Angel of Peace will find its wings and rush over our beloved country. This had been a cold but beautiful day in the morning was very busy on office work. recieved a letter from [illegible], answered the same, in the evening. also wrote short letter to my cousin with the mount pleasant hospital, in the afternoon was visited by my friend Mr. Mom and two members of my Regt. was not out of the hospital today. sent this morning Chronicle to Aunt Mercy.')
+        #obj = json.load(open(self.test_component_with_transcript))
+        #metadata = self.dh.get_component_metadata(obj)
+        #self.assertEqual(metadata['transcription'], u'[written above date]:Stanton Hospital, Washington. City D.C.\n\nThe old year has gone, gone forever with all its joys, sorrows, and cares, a New Year is just began, our hearts beat high with thankfulness and of hope for the future, and we begin the New Year with a merry greeting to all, especially to our absent comrads in the field, may the God of Battles protect all of them and ere another New Years day resore them again to our waiting anxious friends. - a greeting to our bereaved ones with tender sympathy. Those who mourn the loss of these gone forth-to-battle never to return, - to our country in I all levels of Freedom a greeting  with the wish that soon the Angel of Peace will find its wings and rush over our beloved country. This had been a cold but beautiful day in the morning was very busy on office work. recieved a letter from [illegible], answered the same, in the evening. also wrote short letter to my cousin with the mount pleasant hospital, in the afternoon was visited by my friend Mr. Mom and two members of my Regt. was not out of the hospital today. sent this morning Chronicle to Aunt Mercy.')
 
     def test_fetch_harvestable_not_nested(self):
         ''' test that a non-nested object is identified as harvestable '''
@@ -113,9 +113,9 @@ class DeepHarvestNuxeoSimpleTestCase(unittest.TestCase):
     def test_fetch_harvestable_nested(self):
         ''' test that nested objects are identified as harvestable '''
         # FIXME bad test
-        organizational_obj = json.load(open(self.test_nuxeo_organizational_object))
-        harvestable = self.dh.fetch_harvestable(organizational_obj)
-        self.assertEqual(len(harvestable), 50)
+        #organizational_obj = json.load(open(self.test_nuxeo_organizational_object))
+        #harvestable = self.dh.fetch_harvestable(organizational_obj)
+        #self.assertEqual(len(harvestable), 50)
 
     def test_fetch_harvestable_empty_folder(self):
         ''' test that an empty folder doesn't return any harvestable objects '''
