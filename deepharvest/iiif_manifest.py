e985450f01bd61792c1ae89d4c4467e195a4365c
Barbara Hui <barbara.hui@ucop.edu>
2017-07-17 10:39:12 -0700

    Add comments

--- deepharvest/iiif_manifest.py
+++ deepharvest/iiif_manifest.py
@@ -11,8 +11,24 @@ class IIIFManifest():
         '''  Given an object and its components, create a json-ld representation
              compliant with these specs: http://iiif.io/api/presentation/2.1/
         '''
-        pass
+        iiif_manifest_md = {}
 
+        ## manifest: @context
+        # descriptive info: label, metadata (pairs), description, thumbnail, attribution, license, logo
+        # technical/presentation info: @id, @type, format, height, width, viewingDirection, viewingHint
+        # rights info: attribution, license, log
+        # links (?)
+
+
+        ## sequences (for complex objects only?)
+        # descriptive info: must have a label if there is more than one sequence, so probably a good default
+ 
+        ## canvas
+        # descriptive info: label
+
+        ## content
+        
+        # paging - for complex objects?
 def main(argv=None):
     pass
 
